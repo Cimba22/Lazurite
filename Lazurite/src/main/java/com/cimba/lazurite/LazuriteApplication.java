a64a37e21013ab62df69dd0d43b567e7ba79be48
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2024-03-25 16:45:04 +0100

    Starting structure of project.

---
+++ Lazurite/src/main/java/com/cimba/lazurite/LazuriteApplication.java
@@ -0,0 +1,13 @@
+package com.cimba.lazurite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazuriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LazuriteApplication.class, args);
+    }
+
+}
---
+++ Lazurite/src/test/java/com/cimba/lazurite/LazuriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cimba.lazurite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazuriteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
