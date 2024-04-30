ddf4b6466a2f1df90e597b73e6725b7e66a89aa4
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2024-04-30 18:17:13 +0200

    Service for user with CRUD methods.

--- backend/src/main/java/com/cimba/lazurite/service/UserService.java
+++ backend/src/main/java/com/cimba/lazurite/service/UserService.java
@@ -1,7 +1,51 @@
 package com.cimba.lazurite.service;
 
-import org.springframework.stereotype.Service;
+import com.cimba.lazurite.entity.dto.UserDto;
 
-@Service
-public class UserService {
+import java.util.List;
+
+/**
+ * Service interface for managing users.
+ */
+public interface UserService {
+    /**
+     * Registers a new user.
+     *
+     * @param userDto the user data
+     * @throws RegistrationException if an error occurs during registration
+     */
+    void registerUser(UserDto userDto) throws RegistrationException;
+
+    /**
+     * Retrieves all users.
+     *
+     * @return a list of all users
+     */
+    List<UserDto> getAllUsers();
+
+    /**
+     * Retrieves a user by their identifier.
+     *
+     * @param userId the user identifier
+     * @return the user data
+     */
+    UserDto getUserById(Long userId);
+
+    /**
+     * Updates user data.
+     *
+     * @param userId  the user identifier
+     * @param userDto the new user data
+     * @throws UserNotFoundException if the user with the specified identifier is not found
+     */
+    void updateUser(Long userId, UserDto userDto) throws UserNotFoundException;
+
+    /**
+     * Deletes a user.
+     *
+     * @param userId the user identifier
+     * @throws UserNotFoundException if the user with the specified identifier is not found
+     */
+    void deleteUser(Long userId) throws UserNotFoundException;
 }
+
