e2ddc44effb5a863bd97c2343bd43602791cf472
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2024-06-06 23:32:45 +0200

    Implementation of wishlist-card

--- frontend/src/app/modules/wishlist/components/wishlist-card/wishlist-card.component.ts
+++ frontend/src/app/modules/wishlist/components/wishlist-card/wishlist-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {WishlistResponse} from "../../../../services/models/wishlist-response";
 
 @Component({
@@ -11,7 +11,7 @@ export class WishlistCardComponent {
     if(this._wishlist.image){
       return 'data:image/jpg;base64, ' + this.wishlist.image;
     }
-    return this._wishlistCover;
+    return 'https://source.unsplash.com/user/c_v_r/1900x800';
   }
   get wishlist(): WishlistResponse {
     return this._wishlist;
@@ -24,4 +24,24 @@ export class WishlistCardComponent {
   private _wishlist: WishlistResponse = {};
   private _wishlistCover: string | undefined;
 
+  @Output() private delete: EventEmitter<WishlistResponse> = new EventEmitter<WishlistResponse>();
+  @Output() private edit: EventEmitter<WishlistResponse> = new EventEmitter<WishlistResponse>();
+  @Output() private details: EventEmitter<WishlistResponse> = new EventEmitter<WishlistResponse>();
+  @Output() private share: EventEmitter<WishlistResponse> = new EventEmitter<WishlistResponse>();
+
+  onShowDetails() {
+    this.details.emit(this._wishlist);
+  }
+
+  onEdit() {
+    this.edit.emit(this._wishlist);
+  }
+
+  onShare() {
+    this.share.emit(this._wishlist);
+  }
+
+  onDelete() {
+    this.delete.emit(this._wishlist);
+  }
 }
--- frontend/src/app/modules/wishlist/wishlist-routing.module.ts
+++ frontend/src/app/modules/wishlist/wishlist-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainComponent} from "./pages/main/main.component";
 import {WishlistComponent} from "./pages/wishlist/wishlist.component";
+import {MyGiftsComponent} from "./pages/my-gifts/my-gifts.component";
+import {ManageWishlistComponent} from "./pages/manage-wishlist/manage-wishlist.component";
 
 const routes: Routes = [
   {
@@ -11,6 +13,18 @@ const routes: Routes = [
       {
         path: '',
         component: WishlistComponent
+      },
+      {
+        path: 'my-gifts',
+        component: MyGiftsComponent
+      },
+      {
+        path: 'manage',
+        component: ManageWishlistComponent
+      },
+      {
+        path: 'manage/:wishlistId',
+        component: ManageWishlistComponent
       }
     ]
   }
