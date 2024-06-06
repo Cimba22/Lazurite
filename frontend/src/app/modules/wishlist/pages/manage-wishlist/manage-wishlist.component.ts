84874f1ddf806256d06952e5a4ec386ae601fc72
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2024-06-06 23:32:03 +0200

    Implementation of manage-wishlist

---
+++ frontend/src/app/modules/wishlist/pages/manage-wishlist/manage-wishlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-wishlist',
+  templateUrl: './manage-wishlist.component.html',
+  styleUrls: ['./manage-wishlist.component.css']
+})
+export class ManageWishlistComponent {
+  errorMsg: Array<string> = [];
+
+}
